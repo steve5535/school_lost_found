d1b8f2aedcbd45e84bef60cc58a692ab1e84e5b4
steve5535 <steve5535@naver.com>
2026-06-13 19:58:12 +0900

    분실물에 상태추가

--- school_lost_fount.py
+++ school_lost_fount.py
@@ -4,27 +4,30 @@
 # 검색
 # 수정
 
+'''
+lost_items_db 형식
+{
+    1: {"이름":???, "장소":???, "상태:True"},
+    2: {"이름":???, "장소":???, "상태:Flase"}
+}
+'''
+
 
 ## Srvice
 # 분실물 추가 함수
-def add_lost_item(lost_items_db, ID, item, place):
-    lost_items_db[ID] = {"이름":item, "장소":place} # 분실물이름과 장소 딕셔너리로 저장한 값을 전체 분실물 딕셔너리에 추가
-'''
-{
-    1: {"이름":???, "장소":???},
-    2: {"이름":???, "장소":???}
-}
-'''
+def add_lost_item(lost_items_db, item_id, item, place, state):
+    lost_items_db[item_id] = {"이름":item, "장소":place, "상태":state} # 분실물이름과 장소 딕셔너리로 저장한 값을 전체 분실물 딕셔너리에 추가
 
 # 분실물 전체 검색 함수
 def search_all_item(lost_items_db):
     item_box = []
     for item_id, item_info in lost_items_db.items():
-        item_box.append(
-                        {"id":item_id,
+        item_box.append({
+                         "id":item_id,
                          "이름":item_info["이름"],
-                         "장소":item_info["장소"]}
-                        )
+                         "장소":item_info["장소"],
+                         "상태":item_info["상태"]
+                        })
     return item_box
 
 # 분실물 이름으로 검색
@@ -34,12 +37,12 @@ def search_name_item(input_item, lost_items_db):
     for item_id, item_info in lost_items_db.items():
         item_name = item_info["이름"]
         if input_item in item_name:
-            item_box.append(
-                            {"id":item_id,
+            item_box.append({
+                             "id":item_id,
                              "이름":item_info["이름"],
-                             "장소":item_info["장소"]
-                             }
-                            )
+                             "장소":item_info["장소"],
+                             "상태":item_info["상태"]
+                            })
             is_in_item = True
     return is_in_item, item_box
             
@@ -48,7 +51,7 @@ def search_name_item(input_item, lost_items_db):
 # 메인함수
 def main():
     lost_items_db = {}
-    ID = 1
+    item_id = 1
     
     while True:
         print("=============================")
@@ -67,8 +70,9 @@ def main():
         if input_num == 1: 
             item = input("분실물 이름을 작성하세요> ")
             place = input("분실물을 습득한 장소 작성하세요> ")
-            add_lost_item(lost_items_db, ID, item, place)
-            ID += 1
+            state = True
+            add_lost_item(lost_items_db, item_id, item, place, state)
+            item_id += 1
         
         # 전체 분실물 검색
         elif input_num == 2: 
@@ -77,7 +81,7 @@ def main():
             else: # 있다면 보여주기
                 items_list = search_all_item(lost_items_db)
                 for item in items_list:
-                    print(f"{item['id']}. 이름: {item['이름']}, 찾은 장소: {item['장소']}")
+                    print(f"{item['id']}. 이름: {item['이름']}, 찾은 장소: {item['장소']}, 상태: {item['상태']}")
         
         # 분실물 이름으로 검색
         elif input_num == 3: 
@@ -89,9 +93,9 @@ def main():
                 if is_found:
                     print(f"{input_item}이(가) 포함된 분실물을 찾았습니다")
                     for item in found_list:
-                        print(f"{item['id']}. 이름: {item['이름']}, 찾은 장소: {item['장소']}")
+                        print(f"{item['id']}. 이름: {item['이름']}, 찾은 장소: {item['장소']}, 상태: {item['상태']}")
                 else:
-                    print(f"현재 있는 분실물중 \"{input_item}\"은(는) 없습니다")
+                    print(f"현재 있는 분실물중 '{input_item}'은(는) 없습니다")
         
         # 종료
         elif input_num == 4: 
