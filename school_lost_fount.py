3d3caba5c5b0a677c91ee5964b1d6fa531c200c0
steve5535 <steve5535@naver.com>
2026-06-13 14:28:29 +0900

    분실물 전체 검색 기능 수정

--- school_lost_fount.py
+++ school_lost_fount.py
@@ -15,12 +15,23 @@ def add_lost_item(lost_items_db, id, item, place):
 '''
 
 
-# 분실물 검색 함수
-def search_all_item(id, lost_items_db):
+# 분실물 전체 검색 함수
+def search_all_item(lost_items_db):
+    item_box = []
     for i in range(len(lost_items_db)):
         item_name = lost_items_db[i+1]["이름"]
         item_place = lost_items_db[i+1]["장소"]
-        print(f"{id}. 물건이름: {item_name}, 찾은장소: {item_place}")
+        item_box.append([i+1, item_name, item_place])
+    return item_box
+
+# 분실물 이름으로 검색
+def search_name_item(input_item, lost_items_db):
+    for i in range(len(lost_items_db)):
+        item_name = lost_items_db[i+1]["이름"]
+        if input_item == item_name:
+            lost_items_db[i+1]
+            
+        
 
 # 메인함수
 def main():
@@ -31,20 +42,36 @@ def main():
         print("=============================")
         print("1. 분실물 등록")
         print("2. 전체 분실물 검색")
-        print("3. 종료")
+        print("3. 분실물 이름으로 검색")
+        print("4. 종료")
         input_num = int(input("번호 입력> "))
         
-        if input_num == 1:
+        # 분실물 등록
+        if input_num == 1: 
             item = input("분실물 이름을 작성하세요> ")
             place = input("분실물을 습득한 장소 작성하세요> ")
             add_lost_item(lost_items_db, id, item, place)
             id += 1
-        elif input_num == 2:
+        
+        # 전체 분실물 검색
+        if input_num == 2: 
             if len(lost_items_db) == 0: # 등록된 분실물이 있는지 검사
                 print("아직 등록된 분실물이 없습니다")
             else: # 있다면 보여주기
-                search_all_item(id-1, lost_items_db)
-        elif input_num == 3:
+                items_list = search_all_item(lost_items_db)
+                for req_id, req_name, req_place in items_list:
+                    print(f"{req_id}. 이름: {req_name}, 찾은 장소: {req_place}")
+        
+        # 분실물 이름으로 검색
+        if input_num == 3: 
+            input_item = input("분실물 이름을 입력하세요> ")
+            if len(lost_items_db) == 0: # 등록된 분실물이 있는지 검사
+                print("아직 등록된 분실물이 없습니다")
+            else:
+                search_name_item(input_item, lost_items_db)
+        
+        # 종료
+        if input_num == 4: 
             print("프로그램을 종료합니다")
             break
 
